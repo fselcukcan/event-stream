65c51bb9ec41c351f725811b4ca3ce210e23d64d
F. Selçuk Can <5363259+fselcukcan@users.noreply.github.com>
2021-10-23 13:23:44 +0300

    Update event-stream.js

--- event-stream.js
+++ event-stream.js
@@ -18,11 +18,3 @@ function makeEventStream() {
     dispatchEvent,
   }
 }
-
-es = makeEventStream()
-es.addEventListener("click", e => console.log("click called: ", e))
-es.addEventListener("negotiationneeded", e => console.log("negotiationneeded called: ", e))
-es.addEventListener("click", e => console.log("click called: ", e.detail))
-
-var clickEvent = new CustomEvent("click", { detail: 7 })
-es.dispatchEvent(clickEvent)
